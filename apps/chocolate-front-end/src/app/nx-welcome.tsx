3dee1c1ed36d58e9116463b5943dfa063ba5a413
Islam <islamkwnd@gmail.com>
2022-08-06 18:56:14 +0000

    feat(setup): Add small api test

--- apps/chocolate-front-end/src/app/nx-welcome.tsx
+++ apps/chocolate-front-end/src/app/nx-welcome.tsx
@@ -1,24 +1,58 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
-import { useEffect } from 'react';
-async function testApi() {
+import {  useState } from 'react';
+
+
+let $api: ApiPromise | null = null;
+async function testApi(
+  setGenesis: React.Dispatch<React.SetStateAction<string>>,
+  setApiReady: React.Dispatch<React.SetStateAction<boolean>>
+) {
   // Import
 
   // Construct
-  const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+  const wsProvider = new WsProvider('ws://127.0.0.1:8844');
   const api = await ApiPromise.create({ provider: wsProvider });
+  console.log('Established ApiPromise');
   // Do something
-  console.log(api.genesisHash.toHex());
+  await api.isReadyOrError
+    .then((_api) => {
+      setApiReady(true);
+      setGenesis(_api.genesisHash.toHex());
+    })
+    .catch(() => setApiReady(false));
+  const handleDis = () => {
+    setApiReady(false);
+    setGenesis('');
+  };
+  api.on('disconnected', handleDis);
+
+  $api = api;
 }
+
 export function NxWelcome({ title }: { title: string }) {
-  useEffect(() => {
-    testApi();
+  const [apiReady, setApiReady] = useState(false);
+  const [genesis, setGenesis] = useState('');
+  const ready = 'Api is Ready';
+  const nr = 'Api is not Ready';
+  return (
+    <main>
+      <h1>Api Stats:</h1>
 
-    return () => {
-      // second
-    };
-  });
-
-  return <h1>Hi there, I'll be testing polkadot with this component</h1>;
+      <p data-test-id="test-api-p">
+        Ready State: {apiReady ? ready : nr} <br />
+        Genesis Hash: {genesis}
+      </p>
+      <button
+        data-test-id="test-api-btn"
+        onClick={() => {
+          if ($api) $api.disconnect();
+          testApi(setGenesis, setApiReady);
+        }}
+      >
+        Test Api
+      </button>
+    </main>
+  );
 }
 
 export default NxWelcome;
