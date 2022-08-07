08d0e1f6dc5e094f4c6a8fccabf5269a8dc155df
Islam <islamkwnd@gmail.com>
2022-08-07 09:55:51 +0000

    feat(tgen): Add metadata fetch/gen

--- tools/generators/typegen/index.ts
+++ tools/generators/typegen/index.ts
@@ -1,11 +1,45 @@
-import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
+import {
+  Tree,
+  formatFiles,
+  installPackagesTask,
+  readProjectConfiguration,
+  generateFiles,
+} from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
+import { join } from 'path';
+
+import { w3cwebsocket as WebSocket } from 'websocket';
+
+async function fetchMetadata() {
+  const endpoint = 'ws://127.0.0.1:8844';
+  console.log('Connecting to ', endpoint);
+  const ws = new WebSocket(endpoint);
+  const promis = new Promise<string>((res, rej) => {
+    ws.onopen = (): void => {
+      ws.send(
+        '{"id":"1","jsonrpc":"2.0","method":"state_getMetadata","params":[]}'
+      );
+    };
+    ws.onmessage = (msg: any): void => {
+      const metadata = JSON.parse(msg.data).result;
+      // fs.writeFileSync('packages/types/src/metadata/static-latest.ts', `export default '${metadata}'`);
+      res(metadata as string);
+      ws.close();
+    };
+  });
+  const metadata = await promis;
+  return metadata;
+}
 
 export default async function (tree: Tree, schema: any) {
   // Don't create a library
-  await libraryGenerator(tree, { name: schema.name });
-  await formatFiles(tree);
-  // First, update metadata.json in the types library
+  const libraryRoot = readProjectConfiguration(tree, schema.name).root;
+  // First, update metadata.ts in the types library
+  const metadata = await fetchMetadata();
+  generateFiles(tree, join(__dirname, 'files'), libraryRoot, {
+    tmpl: '',
+    metadata,
+  });
   return () => {
     installPackagesTask(tree);
   };
