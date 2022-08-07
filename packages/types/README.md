a3737a912e1bd8ed8b2a90a9dd2419cf7f7684d3
Islam <islamkwnd@gmail.com>
2022-08-07 09:27:05 +0000

    feat(setup): Add types package

---
+++ packages/types/README.md
@@ -0,0 +1,14 @@
+# types
+
+This library includes two things:
+
+1. Api augments for the polkadot api.
+2. Type augments for the polkadot api.
+
+Following the guide on the polkadot docs as of August 7th 2022.
+
+
+## Building
+
+Run `nx build types` to build the library.
+
--- tools/generators/typegen/index.ts
+++ tools/generators/typegen/index.ts
@@ -2,6 +2,7 @@ import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
 export default async function (tree: Tree, schema: any) {
+  // Don't create a library
   await libraryGenerator(tree, { name: schema.name });
   await formatFiles(tree);
   // First, update metadata.json in the types library
