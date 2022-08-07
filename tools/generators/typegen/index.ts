01a03553db003b5851e43b7027c9b0150712bbed
Islam <islamkwnd@gmail.com>
2022-08-07 10:47:34 +0000

    feat(setup): Generator for definitions

--- tools/generators/typegen/index.ts
+++ tools/generators/typegen/index.ts
@@ -7,8 +7,9 @@ import {
 } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 import { join } from 'path';
-
 import { w3cwebsocket as WebSocket } from 'websocket';
+import { generateInterfaceTypes } from '@polkadot/typegen/generate/interfaceRegistry';
+import { generateTsDef } from '@polkadot/typegen/generate/tsDef';
 
 async function fetchMetadata() {
   const endpoint = 'ws://127.0.0.1:8844';
@@ -30,16 +31,45 @@ async function fetchMetadata() {
   const metadata = await promis;
   return metadata;
 }
-
+interface ModWithRpc {
+  name: string;
+  rpc: Record<string, string>;
+}
+const mods: (string | ModWithRpc)[] = ['chocolateModule', 'usersModule'];
 export default async function (tree: Tree, schema: any) {
   // Don't create a library
   const libraryRoot = readProjectConfiguration(tree, schema.name).root;
   // First, update metadata.ts in the types library
   const metadata = await fetchMetadata();
-  generateFiles(tree, join(__dirname, 'files'), libraryRoot, {
-    tmpl: '',
-    metadata,
-  });
+  generateFiles(
+    tree,
+    join(__dirname, 'files/static'),
+    join(libraryRoot, 'static'),
+    {
+      tmpl: '',
+      metadata,
+      // mods,
+    }
+  );
+  // Then update type defs
+  // WIll need to separate outer definitions from mod definitions. It's being regenerated on each run.
+  for (let modname of mods) {
+    let rpc = {};
+    if (typeof modname !== 'string') rpc = modname.rpc;
+    generateFiles(
+      tree,
+      join(__dirname, 'files/src/interfaces'),
+      join(libraryRoot, 'src/interfaces'),
+      {
+        tmpl: '',
+        modname,
+        mods,
+        types: JSON.stringify({}),
+        rpc: JSON.stringify(rpc),
+      }
+    );
+  }
+  await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
   };
