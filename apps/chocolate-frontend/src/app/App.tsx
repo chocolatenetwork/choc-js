dfc5694e4df2c706e5c0300b300dd4aabaff5b6f
Islam <islamkwnd@gmail.com>
2023-02-10 21:29:13 +0100

    initial commit

---
+++ apps/chocolate-frontend/src/app/App.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="chocolate-frontend" />
+    </StyledApp>
+  );
+}
+
+export default App;
+
+if (import.meta.vitest) {
+  // add tests related to your file here
+  // For more information please visit the Vitest docs site here: https://vitest.dev/guide/in-source.html
+
+  const { it, expect, beforeEach } = import.meta.vitest;
+  let render: any;
+
+  beforeEach(async () => {
+    render = (await import('@testing-library/react')).render;
+  });
+
+  it('should render successfully', () => {
+    const { baseElement } = render(<App />);
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should have a greeting as the title', () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/Welcome chocolate-frontend/gi)).toBeTruthy();
+  });
+}
