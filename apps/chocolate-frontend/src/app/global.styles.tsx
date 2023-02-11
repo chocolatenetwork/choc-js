d4c851db63b68c640bbdfc8692d9850098cb2216
Islam <islamkwnd@gmail.com>
2023-02-11 19:22:30 +0100

    feat: Add icon

--- apps/chocolate-frontend/src/app/global.styles.tsx
+++ apps/chocolate-frontend/src/app/global.styles.tsx
@@ -2,7 +2,22 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 
+* ,*::before , *::after{
+  box-sizing: border-box;
+}
+
+img, picture, svg, video {
+  display: block;
+  max-width: 100%;
+}
+
+
+* {
+  margin: 0;
+  padding: 0;
+}
 
 `;
+
 
 export default GlobalStyle;
