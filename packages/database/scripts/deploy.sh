eec6e4974f8fd33ec02227820f1c7bc4452a3d87
Islam <islamkwnd@gmail.com>
2023-04-21 23:30:39 +0100

    feat(db): Add deploy functions option

--- packages/database/scripts/deploy.sh
+++ packages/database/scripts/deploy.sh
@@ -6,5 +6,9 @@ function pushMigrations(){
   nx run database:supabase link --project-ref $SUPABASE_PROJECT_ID
   nx run database:supabase db push
 }
+function deployFunctions(){
+  nx run database:supabase functions deploy app --project-ref $SUPABASE_PROJECT_ID
+}
 
 pushMigrations
+deployFunctions
