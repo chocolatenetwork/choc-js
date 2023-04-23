01f1f437a3ddc3603ac8503257afd709d00c42ca
Islam <islamkwnd@gmail.com>
2023-04-23 10:46:09 +0100

    feat(auth): Add. netlify env variables and correct projectIds

--- packages/database/scripts/deploy.sh
+++ packages/database/scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-
+function preDeploy(){
+  supabase secrets set APP_ENV=$SUPABASE_APP_ENV
+}
 function pushMigrations(){
   nx run database:supabase link --project-ref $SUPABASE_PROJECT_ID
   nx run database:supabase db push
@@ -10,5 +12,6 @@ function deployFunctions(){
   nx run database:supabase functions deploy app --project-ref $SUPABASE_PROJECT_ID
 }
 
+preDeploy
 pushMigrations
 deployFunctions
